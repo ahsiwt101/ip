c2c8379f9fc85bd9610122648c15d8585e54b06d
Twisha <twisha1.mehta@gmail.com>
2026-08-25 11:13:45 +0800

    changed name

--- src/main/java/Milo.java
+++ src/main/java/Milo.java
@@ -1,10 +1,14 @@
-public class Duke {
+/**
+ * Entry point of the Milo chatbot.
+ * For now it only greets the user by printing an ASCII-art banner.
+ */
+public class Milo {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String banner = " __  __  _  _         \n"
+                + "|  \\/  |(_)| |  ___  \n"
+                + "| |\\/| || || | / _ \\ \n"
+                + "| |  | || || || (_) |\n"
+                + "|_|  |_||_||_| \\___/ \n";
         System.out.println(banner);
     }
 }
